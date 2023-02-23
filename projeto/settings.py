11042b021db9c4c6aefcf3a2adfe01204a2b139c
Rodrigo Argenton <rodrigolk4321@gmail.com>
2023-02-23 10:54:02 -0400

    att input search - home

--- projeto/settings.py
+++ projeto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(1q!m4^0ld63x(zkw706jb^(tsp7v@j8#7pn@l*h&bx361^&)v',
+SECRET_KEY = 'django-insecure-(1q!m4^0ld63x(zkw706jb^(tsp7v@j8#7pn@l*h&bx361^&)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
