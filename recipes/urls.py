6bed28895831a2c1f800bfa45481ac1532095fd7
Rodrigo Argenton <rodrigolk4321@gmail.com>
2023-03-07 07:32:15 -0400

    comment on views and urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from recipes import views
 
+# comando para criar uma url, o mesmo detém as duas configurações criadas no arquivo views
 urlpatterns = [
     path('', views.home),
     path('recipes/<int:id>/', views.recipe)
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-
+# função que recebe uma request e renderiza um aquivo no caminho: 'recipes/pages/home.html'
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
     })
+# função que recebe uma request e renderiza um aquivo no caminho: 'recipes/pages/home.html'
+# o parametro id rece um valor inteiro e repassa como forma de separar as receiras após o caminho: 'dominio/recipes/id/'
 def recipe(request, id):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
